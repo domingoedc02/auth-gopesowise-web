5d44973c846085617455d8c3945a52e31795d897
EdsonJohn <edson.john.domingo@ibm.com>
2024-12-21 16:33:36 +0900

    updated v1

--- src/Verification.js
+++ src/Verification.js
@@ -18,16 +18,15 @@ export default function Verification(){
 
     useEffect(() => {
         if ( token === undefined || codeId === undefined ){
-            navigate("/hello")
+            navigate("https://gopesowise.com")
         }
         fetchData()
     }, [token, codeId]);
 
     const fetchData = async () => {
         try {
-            const response = await axios.get(`http://localhost:8080/api/auth/verify/email/link?id=${codeId}&token=${token}`);  // Make GET request
+            const response = await axios.get(`https://dev.server.gopesowise.com/api/auth/verify/email/link?id=${codeId}&token=${token}`);  // Make GET request
             if (response.status !== 200){
-
                 throw new Error(response.statusText)
             }
             setSuccessMessage(response.data.statusText)
